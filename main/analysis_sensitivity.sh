ccf3ded1c64f95c6ffad17b811ca02a6948411d8
linnykos <klin1234@gmail.com>
2021-08-27 18:21:14 -0400

    Fixing typo

--- main/analysis_sensitivity.sh
+++ main/analysis_sensitivity.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$ -N network_sosd_sensitivity
 #$ -j y
-#$ -o .../../../out/networkSoSD/qsub/
+#$ -o ../../../out/networkSoSD/qsub/
 #$ -l m_mem_free=15G
 
 Rscript --no-save analysis_sensitivity.R
